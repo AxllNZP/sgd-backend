3b11a30a3cbff6439c4eb0e5ea70b8de80732ba6
AxllNZP <axellzurita1003@gmail.com>
2026-02-27 11:08:55 -0500

    BACKEND

--- src/main/java/com/mesapartes/sgd/config/SecurityConfig.java
+++ src/main/java/com/mesapartes/sgd/config/SecurityConfig.java
@@ -51,11 +51,9 @@ public class SecurityConfig {
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
-        return source -> {
-            UrlBasedCorsConfigurationSource urlSource = new UrlBasedCorsConfigurationSource();
-            urlSource.registerCorsConfiguration("/**", configuration);
-            return urlSource.getCorsConfiguration(source);
-        };
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
